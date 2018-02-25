3c78c09e2412fe1db0b8aacacf45637438390c30
jackson1442 <jacksonw1442@gmail.com>
2018-02-24 23:58:58 -0600

    attach unique id to each action

--- main.py
+++ main.py
@@ -8,23 +8,29 @@ import praw, time
 r = praw.Reddit('murderedbybots', user_agent="beta v5.2, orange")
 # poltags = ['[pol]', '[political]', '[politics]', '[politician]']
 
+def uniqid():
+    return hex(int(time()*10000000))[2:]
+
+
 murderScore = 30
 burnScore = -30
 modAlert = -80
 clearComment = 80
-commentText = '''**Please help us decide if this is a murder or a burn.** \n\nIf you believe this post is a murder, please upvote *this comment*. \n\nIf you believe this post is a burn, please downvote *this comment*. \n\nIf you believe this post does not belong on this sub, please downvote *the parent post.* and report it if you think the mods need to see it.\n\n*^^I'm ^^a ^^bot, ^^and ^^this ^^action ^^was ^^performed ^^automatically. ^^If ^^you ^^have ^^any ^^questions, ^^please [^^contact ^^the ^^moderators ^^of ^^this ^^subreddit.](https://www.reddit.com/message/compose?to=%2Fr%2FMurderedByWords&subject=&message=)*\n\n'''
+commentText = '''**Please help us decide if this is a murder or a burn.**  \nIf you believe this post is a murder, please upvote *this comment*.  \nIf you believe this post is a burn, please downvote *this comment*. \n\nIf you believe this post does not belong on this sub, please downvote *the parent post.* and report it if you think the mods need to see it.\n\n*^^I'm ^^a ^^bot, ^^and ^^this ^^action ^^was ^^performed ^^automatically. ^^If ^^you ^^have ^^any ^^questions, ^^please [^^contact ^^the ^^moderators ^^of ^^this ^^subreddit.](https://www.reddit.com/message/compose?to=%2Fr%2FMurderedByWords&subject=&message=)*\n\n'''
 burnComment = commentText + '\n\nThis post has successfully been marked as a `Burn`. This *can* still change, depending on votes.'
 murderComment = commentText + '\n\nThis post has successfully been marked as a `Murder`. This *can* still change, depending on votes.'
 contentBanText = '#Be advised, if this post has content relating to nra/guns/school shooting you will be banned for 14 days.  \nPlease remove your post if it falls into these categories and no action will be taken. :)'
-communityFeedback = "\n---\nIf you want to talk about the subreddit, feel free to send us a message in [our official Discord server](https://discord.gg/Fe3eUb6)!"
+footer = "\n\n---\nIf you want to talk about the subreddit, feel free to send us a message in [our official Discord server](https://discord.gg/Fe3eUb6)!\n\n^^[faq](https://www.reddit.com/r/1442dump/wiki/murderedbybots-faq) ^^[source](https://github.com/jackson1442/redditBot) ^^action ^^#"
+
 
 f = open("logfile.txt", "a+")
 #--- comment on new posts, hide from /new ---#
 for p in r.subreddit('MurderedByWords').new():
+    actionID = uniqID()
     if time.time() - p.created_utc > 86400: break
    # linktitle = p.title.lower()
    # if any(string in linktitle for string in poltags): p.mod.flair(text='pol')
-    c = p.reply(commentText + "\n---\n" + contentBanText + communityFeedback)
+    c = p.reply(commentText + "\n---\n" + contentBanText + footer + actionID)
     c.mod.distinguish(how='yes', sticky=True)
     c.save()
 #--- kd module not working yet ---#
@@ -36,22 +42,24 @@ for p in r.subreddit('MurderedByWords').new():
     #        p.report("This post may be a repost of: " item.title + "(" + str(item.time) + ") : " + item.link)
     p.hide()
     print('Commented on post id ' + p.id)
-    f.write('\nCommented on post ' + p.permalink)
+    f.write('\nCommented on post ' + p.permalink + ' - ' + actionID)
     c.clear_vote()
 #--- check for unapproved posts ---#
 for u in r.subreddit('MurderedByWords').mod.unmoderated():
     if u.score > 5000:
-        tempRemovalMessage = 'Greetings, /u/'+str(u.author)+'! Your [post]('+u.permalink+') on /r/MurderedByWords has been temporarily removed so a moderator can review it. This prevents low quality content from making the frontpage.\n\nThe moderators have been notified of this action, and will reinstate your post if it belongs here. You will receive a reply regardless of the decision.\n\nIf you have any questions, please [send the moderators a message](https://www.reddit.com/message/compose?to=%2Fr%2FMurderedByWords&subject=Question+about+the+temporary+removal+of+a+post&message= '+u.permalink+') \n\n**Do not send this account a message; it is a bot.** If you want, you can also send the moderators a message in the [MBW discord](https://discord.gg/rEvkEhD)'
-        r.subreddit('MurderedByWords').modmail.create('Post temporarily removed', tempRemovalMessage, str(u.author))
-        c = u.reply(tempRemovalMessage + communityFeedback)
+        actionID = uniqid()
+        tempRemovalMessage = 'Greetings, /u/'+str(u.author)+'! Your [post]('+u.permalink+') on /r/MurderedByWords has been temporarily removed so a moderator can review it. This prevents low quality content from making the frontpage.\n\nThe moderators have been notified of this action, and will reinstate your post if it belongs here. You will receive a reply regardless of the decision.\n\nIf you have any questions, please [send the moderators a message](https://www.reddit.com/message/compose?to=%2Fr%2FMurderedByWords&subject=Question+about+the+temporary+removal+of+a+post&message= '+u.permalink+') \n\n**Do not send this account a message; it is a bot.**'
+        r.subreddit('MurderedByWords').modmail.create('Post temporarily removed', tempRemovalMessage + footer + actionID, str(u.author))
+        c = u.reply(tempRemovalMessage + footer + actionID)
         c.mod.distinguish(how='yes', sticky=True)
         u.mod.remove()
         u.report("Temporarily removed due to upvotes with no approval. Please verify.")
         print('temporarily removed post ' + u.permalink)
-        f.write('\nRemoved post ' + u.permalink + ' temporarily for review')
+        f.write('\nRemoved post ' + u.permalink + ' temporarily for review - ' + actionID)
 time.sleep(10)
 #--- Sort through previously made comments, flair/edit accordingly. ---#
 for c in r.redditor('murderedbybots').saved():
+    actionID = uniqID()
     print(c.parent().permalink)
     if time.time() - c.created_utc > 423000: c.parent().mod.flair(text="Burn"); c.delete(); continue
     if c.stickied != True: c.delete(); continue
@@ -59,23 +67,23 @@ for c in r.redditor('murderedbybots').saved():
     if c.parent().author == '[deleted]': c.delete(); continue
 
     if c.score > murderScore:
-        if c.body != murderComment + communityFeedback:
-            c.edit(murderComment + communityFeedback)
+        if c.body != murderComment + footer:
+            c.edit(murderComment + footer + actionID)
             c.parent().mod.flair(text='Murder')
-            f.write('\nFlaired post ' + c.parent().permalink + ' as Murder')
+            f.write('\nFlaired post ' + c.parent().permalink + ' as Murder - ' + actionID)
         if c.score > clearComment: c.delete()
 
     elif c.score < burnScore:
-        if c.body != burnComment + communityFeedback:
-            c.edit(burnComment + communityFeedback)
+        if c.body != burnComment + footer:
+            c.edit(burnComment + footer + actionID)
             c.parent().mod.flair(text='Burn');
-            f.write('\nFlaired post ' + c.parent().permalink + ' as Burn')
+            f.write('\nFlaired post ' + c.parent().permalink + ' as Burn - ' + actionID)
 
         if c.score < modAlert:
             #--- temporarily disabled modmail feature because it was blowing up and annoying ---#
             # r.subreddit('MurderedByWords').message('Possible LQ content alert', 'The following post has been marked as a burn with a *very* low score.\n\nPlease investigate at your convenience.\n\n' + c.parent().permalink)
-            c.parent().report("Potential low quality content, overwhelmingly voted as burn.")
-            f.write('\nReported post ' + c.parent().permalink + ' as potential low quality')
+            c.parent().report("Potential low quality content, overwhelmingly voted as burn. - " + actionID)
+            f.write('\nReported post ' + c.parent().permalink + ' as potential low quality - ' + actionID)
             c.delete()
     else: continue
     print(c.parent().permalink)
